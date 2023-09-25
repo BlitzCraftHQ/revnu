13b615348c529dacadd3848faf61af5056f1d0c0
Fabian Ferno <fabianferno@gmail.com>
2023-09-25 19:45:06 +0530

    Update events and add addresses.

--- contracts/contracts/RevnuRegistry.sol
+++ contracts/contracts/RevnuRegistry.sol
@@ -23,24 +23,26 @@ contract RevnuRegistry{
     // Struct to represent a Engagement Request entry
     struct Bounty{
         uint256 bountyId;
+        address bountyCreator;
         string actionId;        // videoId, channelId, id, etc
         string actionType;      // like, sub, comment, etc
         uint actionCount;    // no. of likes or subscribers 
-        uint actionCompleted;   // no. of likes or subscribers completed
+        uint noOfClaims;   // no. of likes or subscribers completed
         uint256 reward;         // total amt (no.of likes * amt quoted)
     }
 
     // Struct to represent details of completed requests
     struct Claim{ 
         uint256 bountyId;
+        address claimer;
         bytes32 claimHash;
     }
 
     mapping(uint256 => Bounty) public bountyRegistry;
     mapping(bytes32 => Claim) public claimRegistry;
 
-    event ClaimAdded(uint256 bountyId, bytes32 claimHash);
-    event BountyAdded(uint256 bountyId, string actionId, string actionType, uint actionCount, uint actionCompleted, uint256 reward);
+    event BountyClaimed(uint256 bountyId, address claimer, bytes32 claimHash, uint256 claimReward);
+    event BountyAdded(uint256 bountyId, address bountyCreator, string actionId, string actionType, uint actionCount, uint noOfClaims, uint256 reward);
 
     function createBounty(string memory _actionId, string memory _actionType, uint _actionCount, uint256 _reward) public payable{
         // check balance 
@@ -50,9 +52,9 @@ contract RevnuRegistry{
 
         _bountiesCounter.increment();
         uint256 _bountyId = _bountiesCounter.current();
-        bountyRegistry[_bountyId] = Bounty(_bountyId, _actionId, _actionType, _actionCount, 0, _reward);
+        bountyRegistry[_bountyId] = Bounty(_bountyId, msg.sender, _actionId, _actionType, _actionCount, 0, _reward);
 
-        emit BountyAdded(_bountyId, _actionId, _actionType, _actionCount, 0, _reward);
+        emit BountyAdded(_bountyId, msg.sender, _actionId, _actionType, _actionCount, 0, _reward);
         
         // Deposit reward tokens to contract
         revnuToken.transferFrom(msg.sender, address(this), _reward);
@@ -63,7 +65,7 @@ contract RevnuRegistry{
         require(bountyRegistry[_bountyId].bountyId == _bountyId, "Bounty does not exist.");
 
         // Check if bounty is claimed
-        require(bountyRegistry[_bountyId].actionCompleted < bountyRegistry[_bountyId].actionCount, "Bounty completed.");
+        require(bountyRegistry[_bountyId].noOfClaims < bountyRegistry[_bountyId].actionCount, "Bounty completed.");
 
 
         // Generate claimHash using bountyId and msg.sender
@@ -72,16 +74,16 @@ contract RevnuRegistry{
         // Check if claimHash already exists
         require(claimRegistry[_claimHash].claimHash != _claimHash, "Bounty already claimed by user."); 
 
-        claimRegistry[_claimHash] = Claim(_bountyId, _claimHash);
-        uint256 _reward = bountyRegistry[_bountyId].reward / bountyRegistry[_bountyId].actionCount; 
+        claimRegistry[_claimHash] = Claim(_bountyId, msg.sender, _claimHash);
+        uint256 _claimReward = bountyRegistry[_bountyId].reward / bountyRegistry[_bountyId].actionCount; 
 
-        // Increment Bounty actionCompleted
-        bountyRegistry[_bountyId].actionCompleted += 1;
+        // Increment Bounty noOfClaims
+        bountyRegistry[_bountyId].noOfClaims += 1;
 
-        emit ClaimAdded(_bountyId, _claimHash); 
+        emit BountyClaimed(_bountyId, msg.sender, _claimHash, _claimReward); 
        
         // Transfer amt
-        revnuToken.transfer(msg.sender, _reward);
+        revnuToken.transfer(msg.sender, _claimReward);
     }
 
     function getLatestBountyId() public view returns(uint256){
