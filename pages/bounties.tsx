d2a6d858128bbedd10c911de234735413c2b1f65
veroni-d20 <veronishwetha.23it@licet.ac.in>
2023-09-26 13:23:41 +0530

    list of bounties integeration

--- pages/bounties.tsx
+++ pages/bounties.tsx
@@ -1,22 +1,14 @@
-import ApplicationLayout from "@/components/Utilities/ApplicationLayout";
 import Head from "next/head";
+import ApplicationLayout from "@/components/Utilities/ApplicationLayout";
 import DEPLOYED_CONTRACTS from "@/utilities/contractDetails";
-import { useEffect } from "react";
-import {
-  useContractInfiniteReads,
-  useContractRead,
-  useContractReads,
-} from "wagmi";
-import { useState } from "react";
-
-const revnuContractConfig = {
-  address: DEPLOYED_CONTRACTS.REVNU_REGISTRY.address,
-  abi: DEPLOYED_CONTRACTS.REVNU_REGISTRY.abi,
-  functionName: "bountyRegistry",
-};
+import { useState, useEffect } from "react";
+import { useContractRead } from "wagmi";
 
 export default function Bounties() {
   const [bounties, setBounties] = useState<any>([]);
+
+  //get last bounty ID
+
   const {
     data: lastBountyId,
     isError,
@@ -43,82 +35,26 @@ export default function Bounties() {
     <>
       <Head>
         <title>
-          Dashboard - Revnu | Democratizing EV Technology using the Blockchain
+          Dashboard - Electra | Democratizing EV Technology using the Blockchain
         </title>
-        <meta
-          name="title"
-          content="Notifications - Electra | Push Notification Service for the Neo Blockchain"
-        />
-        <meta
-          name="description"
-          content="Stay informed and in-the-know with real-time push
-            notifications on transactions, smart contracts, and network
-            developments. Empower your Neo experience with Electra's
-            timely alerts, ensuring you never miss a beat on the Neo
-            Blockchain."
-        />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://neocast.blitzcrafthq.com" />
-        <meta
-          property="og:title"
-          content="Notifications - Electra | Push Notification Service for the Neo Blockchain"
-        />
-        <meta
-          property="og:description"
-          content="Stay informed and in-the-know with real-time push
-            notifications on transactions, smart contracts, and network
-            developments. Empower your Neo experience with Electra's
-            timely alerts, ensuring you never miss a beat on the Neo
-            Blockchain."
-        />
-        <meta property="og:image" content="/meta-image.jpg" />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content="https://neocast.blitzcrafthq.com/"
-        />
-        <meta
-          property="twitter:title"
-          content="Notifications - Electra | Push Notification Service for the Neo Blockchain"
-        />
-        <meta
-          property="twitter:description"
-          content="Stay informed and in-the-know with real-time push
-            notifications on transactions, smart contracts, and network
-            developments. Empower your Neo experience with Electra's
-            timely alerts, ensuring you never miss a beat on the Neo
-            Blockchain."
-        />
-        <meta property="twitter:image" content="/meta-image.jpg" />
       </Head>
 
       <ApplicationLayout
         customHeader="Bounties"
-        customHeaderDescription="Electra makes it extremely easy for your to charge your EV at virtually any Electra-enabled charging station."
+        // customHeaderDescription="Here's a list of all recent proposals created on the Electra
+        //     DAO."
       >
-        <div className="rounded-md bg-white px-5 py-6 shadow sm:px-6">
-          <div className="container">
-            {isLoading ? (
-              <p>Loading</p>
-            ) : isSuccess ? (
-              <div>
-                {bounties.map((bountyId: any, i: number) => (
-                  <BountyCard bountyId={bountyId} key={i} />
-                ))}
-              </div>
-            ) : (
-              <p>Error</p>
-            )}
-          </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-5 gap-y-5">
+          {bounties.map((bounty, index) => (
+            <BountyCard bountyId={bounty} key={index} />
+          ))}
         </div>
       </ApplicationLayout>
     </>
   );
 }
 
-function BountyCard({ bountyId }: any) {
+function BountyCard({ bountyId, key }: any) {
   const {
     data: bounty,
     isError,
@@ -131,28 +67,86 @@ function BountyCard({ bountyId }: any) {
     args: [bountyId],
   });
 
-  return (
-    <div>
-      {isLoading ? (
-        "Loading"
-      ) : (
-        <div className="mt-5">
-          Bounty ID: {bounty[0].toString()}
-          <br />
-          Bounty Creator: {bounty[1].toString()}
-          <br />
-          Action ID: {bounty[2].toString()}
-          <br />
-          Action Type: {bounty[3].toString()}
-          <br />
-          Action Count: {bounty[4].toString()}
-          <br />
-          Action Claims: {bounty[5].toString()}
-          <br />
-          {/* Reward for that action */}
-          Reward: {parseFloat(bounty[6]) / parseFloat(bounty[4])}
+  return isLoading ? (
+    "Loading"
+  ) : (
+    <div key={key} className="bg-white border border-gray-200 rounded-md">
+      <div className="px-5 sm:px-6 lg:px-8 pt-5 font-black text-xl">
+        Bounty Information
+      </div>
+      {/* Votes Progress Bar Start */}
+      <div className="mt-5 px-5 sm:px-6">
+        <p className="mt-1 text-md leading-5 font-bold flex justify-between">
+          <span className="text-zinc-700">Action Claims</span>
+          <span className="">
+            {bounty[5].toString()} out of {bounty[4].toString()}
+          </span>
+        </p>
+        <div className="mt-3 w-full bg-zinc-200 rounded-full h-1">
+          <div
+            className="bg-green-600 h-1 rounded-full"
+            style={{
+              width: `${
+                (parseFloat(bounty[5]) / parseFloat(bounty[4])) * 100
+              }%`,
+            }}
+          ></div>
         </div>
-      )}
+      </div>
+      {/* Votes Progress Bar End */}
+      {/* Addresses Start */}
+      <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+        <table className="min-w-full divide-y divide-gray-300">
+          <tbody className="divide-y divide-gray-200">
+            <tr>
+              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                Bounty ID
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
+                {bounty[0].toString()}
+              </td>
+            </tr>
+            <tr>
+              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                Bounty Creator
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
+                {bounty[1].toString()}
+              </td>
+            </tr>
+            <tr>
+              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                Action ID
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
+                {bounty[2].toString()}
+              </td>
+            </tr>
+            <tr>
+              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                Action Type
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
+                {bounty[3].toString()}
+              </td>
+            </tr>
+            <tr>
+              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                Action Rewards
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
+                {parseFloat(bounty[6]) / parseFloat(bounty[4])}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      {/* Addresses End */}
+      <div className="px-5 sm:px-6 pb-5">
+        <button className="w-full rounded-md bg-primary-400 px-8 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-400">
+          Validate
+        </button>
+      </div>
     </div>
   );
 }
