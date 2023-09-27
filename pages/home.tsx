9cd967cda73735c7d32f0702da3e905852ff3168
veroni-d20 <veronishwetha.23it@licet.ac.in>
2023-09-27 20:26:42 +0530

    filter user bounties

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Head from "next/head";
 import axios from "axios";
 import { useAccount, useContractRead, useContractWrite } from "wagmi";
@@ -6,6 +6,7 @@ import DEPLOYED_CONTRACTS from "@/utilities/contractDetails";
 import { XMarkIcon } from "@heroicons/react/20/solid";
 import ApplicationLayout from "@/components/Utilities/ApplicationLayout";
 import "mapbox-gl/dist/mapbox-gl.css";
+import Link from "next/link";
 
 export default function Home() {
   const { address } = useAccount();
@@ -26,6 +27,23 @@ export default function Home() {
     functionName: "getLatestBountyId",
   });
 
+  const {
+    data: balance,
+    isBalanceError,
+    isBalanceLoading,
+    isBalanceSuccess,
+  }: any = useContractRead({
+    address: DEPLOYED_CONTRACTS.REVNU_TOKEN.address,
+    abi: DEPLOYED_CONTRACTS.REVNU_TOKEN.abi,
+    functionName: "balanceOf",
+    args: [address],
+  });
+
+  console.log(balance);
+
+  const bountiesContainerRef: any = useRef(null);
+  const [showEmptyState, setShowEmptyState] = useState(false);
+
   const range: any = (n: number) =>
     Array.from(Array(n).keys()).map((n) => n + 1);
 
@@ -35,7 +53,12 @@ export default function Home() {
       setBounties(bounties);
       console.log("Last Bounty", bounties);
     }
-  }, [lastBountyId]);
+
+    // Show Empty State if there are no bounties
+    if (bountiesContainerRef.current.children.length == 0) {
+      setShowEmptyState(true);
+    }
+  }, [lastBountyId, bountiesContainerRef]);
 
   return (
     <>
@@ -123,29 +146,60 @@ export default function Home() {
               <div className="mt-1 font-medium text-gray-500 text-sm">
                 Tokens you poses in your account
               </div>
-              <div className="mt-5 font-black text-5xl text-gray-900">
-                10{" "}
-                <span className="text-base text-gray-500 font-medium">
-                  RVTK
-                </span>
-              </div>
+              {balance && (
+                <div className="mt-5 font-black text-5xl text-gray-900">
+                  {balance.toString()}{" "}
+                  <span className="text-base text-gray-500 font-medium">
+                    RVTK
+                  </span>
+                </div>
+              )}
             </div>
           </div>
         </div>
         {/* Tokens Balance End */}
         <div className="m-2">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-5 gap-y-5">
+          <div
+            ref={bountiesContainerRef}
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-5 gap-y-5"
+          >
             {bounties.map((bounty, index) => (
-              <BountyCard bountyId={bounty} key={index} />
+              <BountyCard bountyId={bounty} key={index} userOnly={true} />
             ))}
           </div>
+
+          {showEmptyState && (
+            <Link
+              href={"create-bounty"}
+              className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              <svg
+                className="mx-auto h-12 w-12 text-gray-400"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 48 48"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
+                />
+              </svg>
+              <span className="mt-2 block text-sm font-semibold text-gray-900">
+                Create a new bounty
+              </span>
+            </Link>
+          )}
         </div>
       </ApplicationLayout>
     </>
   );
 }
 
-function BountyCard({ bountyId, key }: any) {
+function BountyCard({ bountyId, key, userOnly = false }: any) {
+  const { address } = useAccount();
   const {
     data: bounty,
     isError,
@@ -158,81 +212,81 @@ function BountyCard({ bountyId, key }: any) {
     args: [bountyId],
   });
 
-  return isLoading ? (
-    "Loading"
-  ) : (
-    <div key={key} className="bg-white border border-gray-200 rounded-md">
-      <div className="px-5 sm:px-6 lg:px-8 pt-5 font-black text-xl">
-        Bounty Information
-      </div>
-      {/* Votes Progress Bar Start */}
-      <div className="mt-5 px-5 sm:px-6">
-        <p className="mt-1 text-md leading-5 font-bold flex justify-between">
-          <span className="text-zinc-700">Action Claims</span>
-          <span className="">
-            {bounty[5].toString()} out of {bounty[4].toString()}
-          </span>
-        </p>
-        <div className="mt-3 w-full bg-zinc-200 rounded-full h-1">
-          <div
-            className="bg-green-600 h-1 rounded-full"
-            style={{
-              width: `${
-                (parseFloat(bounty[5]) / parseFloat(bounty[4])) * 100
-              }%`,
-            }}
-          ></div>
+  return address && isLoading
+    ? "Loading"
+    : userOnly && address.toString() == bounty[1].toString() && (
+        <div key={key} className={`bg-white border border-gray-200 rounded-md`}>
+          <div className="px-5 sm:px-6 lg:px-8 pt-5 font-black text-xl">
+            Bounty Information
+          </div>
+          {/* Votes Progress Bar Start */}
+          <div className="mt-5 px-5 sm:px-6">
+            <p className="mt-1 text-md leading-5 font-bold flex justify-between">
+              <span className="text-zinc-700">Action Claims</span>
+              <span className="">
+                {bounty[5].toString()} out of {bounty[4].toString()}
+              </span>
+            </p>
+            <div className="mt-3 w-full bg-zinc-200 rounded-full h-1">
+              <div
+                className="bg-green-600 h-1 rounded-full"
+                style={{
+                  width: `${
+                    (parseFloat(bounty[5]) / parseFloat(bounty[4])) * 100
+                  }%`,
+                }}
+              ></div>
+            </div>
+          </div>
+          {/* Votes Progress Bar End */}
+          {/* Addresses Start */}
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <tbody className="divide-y divide-gray-200">
+                <tr>
+                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                    Bounty ID
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
+                    {bounty[0].toString()}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                    Bounty Creator
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
+                    {bounty[1].toString()}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                    Action ID
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
+                    {bounty[2].toString()}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                    Action Type
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
+                    {bounty[3].toString()}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                    Action Rewards
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
+                    {parseFloat(bounty[6]) / parseFloat(bounty[4])}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          {/* Addresses End */}
         </div>
-      </div>
-      {/* Votes Progress Bar End */}
-      {/* Addresses Start */}
-      <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-        <table className="min-w-full divide-y divide-gray-300">
-          <tbody className="divide-y divide-gray-200">
-            <tr>
-              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                Bounty ID
-              </td>
-              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
-                {bounty[0].toString()}
-              </td>
-            </tr>
-            <tr>
-              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                Bounty Creator
-              </td>
-              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
-                {bounty[1].toString()}
-              </td>
-            </tr>
-            <tr>
-              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                Action ID
-              </td>
-              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
-                {bounty[2].toString()}
-              </td>
-            </tr>
-            <tr>
-              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                Action Type
-              </td>
-              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
-                {bounty[3].toString()}
-              </td>
-            </tr>
-            <tr>
-              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                Action Rewards
-              </td>
-              <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-900 text-right">
-                {parseFloat(bounty[6]) / parseFloat(bounty[4])}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      {/* Addresses End */}
-    </div>
-  );
+      );
 }
